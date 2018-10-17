af1ee1de3e3b03cf0f0dd437e05c94a1e4bb2326
frufrula <l.tgessner@gmail.com>
2018-10-17 05:49:37 -0500

    working on game

--- js/game.js
+++ js/game.js
@@ -1,23 +1,20 @@
-// function setup() { 
-//   createCanvas(windowWidth, windowHeight);
-// } 
+// let pizza;
+function setup() { 
+  // createCanvas(windowWidth, windowHeight);
+  createCanvas(1300, 600)
+  // pizza = new PizzaForm();
+}
 
-// const pizza;
-// const setup = () => {
-//   createCanvas(windowWidth, windowHeight);
-//   pizza = new pizzaForm;
-// }
+function draw() {
+  // backround(0);
+  // pizza.render();
+}
 
-// const draw = () => {
-//   backround(0);
-//   pizza.render;
-// }
-
-// // to let start the Pizza in the middle of the screen, center it at Width/2 and Height/2
-// const pizzaForm = () => {
+// to let start the Pizza in the middle of the screen, center it at Width/2 and Height/2
+// function PizzaForm() {
 //   this.pos = createVector(width/2, height/2);
-//   this.r = 10;
-//   this.render = function () {
+//   this.r = 30;
+//   this.render = function() {
 //     translate(this.pos.x, this.pos.y);
 //     // make a triangle for the pizza
 //     // p5 function with 3 x and y coordinates
